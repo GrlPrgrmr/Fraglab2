ba98434476d5ecd55dbf4cf1697bc35452dac833
Neha261289 <neha.bpit@gmail.com>
2017-02-24 15:07:16 -0800

    Initial commit

---
+++ app/src/main/java/fraglab2/android/csulb/edu/fraglab2/Fragment2.java
@@ -0,0 +1,21 @@
+package fraglab2.android.csulb.edu.fraglab2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Neha on 2/24/2017.
+ */
+
+public class Fragment2 extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //----inflate the layout for this fragment --
+        return inflater.inflate(R.layout.fragment2,container, false);
+    }
+}
